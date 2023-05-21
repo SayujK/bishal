18c5c3664175f2074415c858e487d03e0cb7a493
SayujK <105600499+SayujK@users.noreply.github.com>
2023-05-21 21:15:54 +0545

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+    
+?>
+<html>
+    <head>
+        <link rel="stylesheet" href="./sass/header.css">
+    </head>
+    <body>
+        <header class="header">
+            <a href="html.php"><h1>Attendance Management System</h1></a>
+                <ul class="header-ul">
+                <li><a href="adduser.php">ADD USER</a></li>
+                <li><a href="edituser.php">EDIT USER</a></li>
+                <li><a href="admin.php">Admin Dashboard</a></li>
+                <li><a href="logout.php" class="logout">logout</a></li>
+            </ul>
+        </header>
+    </body>
+</html>
